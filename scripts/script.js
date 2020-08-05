6ccccebbd5b679183a0326dc31e1729b65a8f72d
fmkrom <fmkrom@yandex.ru>
2020-08-05 15:05:23 +0300

    refactor: работа над кодом JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,22 @@
+//1. Переменные для функции открытия и закрытия всплывающего окна
+
 let editProfile = document.querySelector('.profile__edit-button');
 let closePopup = document.querySelector('.popup__form-button-close');
 
 let popUp = document.querySelector('.popup');
 
+let formElement = document.querySelector('.popup__form-content');
+
+//2. Переменные для функции сохранения данных во всплывающем окне
+
+let nameInput = document.querySelector('.popup__form-field-name'); 
+let jobInput = document.querySelector('.popup__form-field-job'); 
+
 function popUpOpen(){
     popUp.classList.remove('popup');
     popUp.classList.add('popup_opened');
-    
+
+    console.log('Function open works!');
 }
 
 editProfile.addEventListener ('click', popUpOpen);
@@ -14,37 +24,38 @@ editProfile.addEventListener ('click', popUpOpen);
 function popUpClose(){
     popUp.classList.remove('popup_opened');
     popUp.classList.add('popup');
+
+    console.log('Function close works!');
 }
 
 closePopup.addEventListener ('click', popUpClose);
 
 
 //Далее - функция сохранения данных
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form-content');
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
 
-    // Находим поля формы в DOM
-    let nameInput = document.querySelector('.popup__form-field-name'); 
-    let jobInput = document.querySelector('.popup__form-field-job'); 
+    /*
+    ВАЖНО! С момента переноса этих переменных:
+    let name = nameInput.value;
+    let job = jobInput.value;
+    
+    за пределы функции - функция работает, но данные перестают сохраняться!
+    */
 
-    // Получите значение полей из свойства value
     let name = nameInput.value;
     let job = jobInput.value;
 
-    // Выберите элементы, куда должны быть вставлены значения полей
     let nameProfile = document.querySelector('.profile__name'); 
     let jobProfile = document.querySelector('.profile__job');
     
-    // Вставьте новые значения с помощью textContent
     nameProfile.textContent = (name);
     jobProfile.textContent = (job);
+
+    console.log('Function save data works!');
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 formElement.addEventListener('submit', popUpClose);
 
