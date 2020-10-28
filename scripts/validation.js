630b4f2786544c1442b2e43e7a86c98ed57a88d6
fmkrom <fmkrom@yandex.ru>
2020-10-28 14:30:01 +0300

    feature: закончить функции валидации форм

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,14 +1,7 @@
-/*=====================================================================================*/
 //Функции валидации форм
 
-/*Первичный рабочий вариант кода: валидация работает, но нет:
-а) Функции переключения кнопки
+//Функция: высветить невалидное поле - работает
 
-*/
-
-/*===============================================================================*/
-
-// Функция свечивания невалидного поля
 function showInputError(formElement, inputElement, errorMessage){
     
   //Находим DOM-элемент с ошибкой по идентификатору:
@@ -24,9 +17,13 @@ function showInputError(formElement, inputElement, errorMessage){
   //Добавляем это поле в DOM:
   errorElement.classList.remove('form__field_hidden');
   errorElement.classList.add('form-error_shown');
+
+  console.log('function showInputError works');
 };
 
-const hideInputError = (formElement, inputElement) => {
+//Функция: убрать подсветку невалидного поля: работает
+
+function hideInputError(formElement, inputElement){
   // Находим элемент ошибки
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   
@@ -40,43 +37,74 @@ const hideInputError = (formElement, inputElement) => {
   
   //Делаем его текст пустым:
   errorElement.textContent = '';
+
+  console.log('function hideInputError works');
 }; 
 
-/*=====================================================================================*/
-//САМА ФУНКЦИЯ ВАЛИДАЦИИ
+//Функция: вернуть при невалидности!
+function returnInvalidInput(inputList){
+  
+  return inputList.some(function(inputElement){
+  // Если поле не валидно, колбэк вернёт true обход массива прекратится и вся фунцкция hasInvalidInput вернёт true
+  console.log('function returnInvalidInput says: form is invalid!');
+  return !inputElement.validity.valid;
+  });
 
-function validateForm(formElement, inputElement){
+}
+
+//Переключение кнопки
+function toggleButtonState(inputList, buttonElement){
+  
+  if (returnInvalidInput(inputList)){
+    buttonElement.classList.add('form__button-save_inactive');
+    buttonElement.disabled = true;
+    console.log('function toggleButtonState: button is disabled');
+
+  } else {
+    buttonElement.classList.remove('form__button-save_inactive');
+    buttonElement.classList.add('form__button-save_active');
+    buttonElement.disabled = false;
+    console.log('function toggleButtonState: button is enabled');
+  }
+}; 
+
+
+
+//Функция: проверка валидности полей - работает (проверено через консоль!)
+function isValid(formElement, inputElement){
+  
   if (!inputElement.validity.valid){
       showInputError(formElement, inputElement, inputElement.validationMessage);
-  }else {
+      console.log('function isValid: field invalid!');
+  } else {
       hideInputError(formElement, inputElement);
+      console.log('function isValid: field valid!');
   }
 };
 
-/*ВАЖНО! В этом варианте кода еще нет функции toggleButtonState - напишем ее!*/
-
-/*===================================================================================*/
-/*ФУНКЦИЯ: добавляем слушатели событий*/
-
+//Функция: добавляем слушатели событий
 function setEventListeners(formElement){
   
   //Делаем массив их всех элементов полей форм:
-  const inputList = Array.from(formElement.querySelectorAll('.form__field'));
+  const inputListArray = Array.from(formElement.querySelectorAll('.form__field'));
+
+  //Добавляем константу кнопки:
+  const buttonToToggle = formElement.querySelector('.form__button-save');
   
-  inputList.forEach(function(inputElement){
+  inputListArray.forEach(function(inputElement){
       
       //Каждому элементу массива полей добавляем обработчик события:
-  
-      inputElement.addEventListener('input', () => {
-        // Внутри колбэка вызовем isValid,
-        // передав ей форму и проверяемый элемент
-        validateForm(formElement, inputElement)
+        inputElement.addEventListener('input', function(){
+        // Внутри колбэка вызовем isValid, передав ей форму и проверяемый элемент
+        isValid(formElement, inputElement);
+        
+        //Применяем переключение кнопки: к массиву полей форм и константе кнопки - т.е. задаем переменные этой функции
+        toggleButtonState(inputListArray, buttonToToggle);
       });
-  
   });
-};
 
-/*ФУНКЦИЯ: обеспечить валидацию форм*/
+  console.log('function setEventListeners works!');
+};
 
 function enableValidation(){
 
@@ -91,90 +119,20 @@ function enableValidation(){
 
       // Для каждой формы вызовем функцию setEventListeners, передав ей элемент формы
       setEventListeners(formElement);
-  });
-};
-
-enableValidation();
-
-/*================================================================================================*/
-//Черновые варианты функций из предыдущих версий
-
-/*
-//САМА ФУНКЦИЯ ВАЛИДАЦИИ
-
-function validateForm(formElement, inputElement){
-  if (!inputElement.validity.valid){
-      showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-      hideInputError(formElement, inputElement);
-  }
-};
-
-function checkValidity(inputList){
-  //Проходимся по списку методом some
-  return inputList.some(function(inputElement){
-      return !inputElement.validity.valid;
-  })
-};
-
-//Функция: переключение кнопки при валидации
-const toggleButtonState = (inputList, buttonElement) => {
-  
-  // Если есть хотя бы один невалидный инпут
-  if (checkValidity(inputList)) {
-    buttonElement.classList.add('form__button-save_inactive');
-    buttonElement.setAttribute('disabled', true);
-  } else {
-    //Иначе - сделаем кнопку активной:
-    buttonElement.classList.add('form__button-save_active');
-    buttonElement.removeAttribute('disabled', true);
-  }
-};
-
-//ФУНКЦИЯ: добавляем слушатели событий
-
-function setEventListeners(formElement){
-  
-  //Делаем массив их всех элементов полей форм:
-  const inputList = Array.from(formElement.querySelectorAll('.form__field'));
-  
-    inputList.forEach(function(inputElement){
-    //Каждому элементу массива полей добавляем обработчик события:
-      inputElement.addEventListener('input', () => {
-        // Внутри колбэка вызовем isValid,
-        // передав ей форму и проверяемый элемент
-        validateForm(formElement, inputElement)
-      });
-
-    toggleButtonState(inputList, buttonElement);
-  
-  });
-};
-
-//ФУНКЦИЯ: обеспечить валидацию форм
-
-function enableValidation(){
-
-  const formList = Array.from(document.querySelectorAll('.form'));
-
-  // Переберём полученную коллекцию - каждому элементу в ней отменим поведение по умолчанию:
-  formList.forEach(function(formElement){
       
-      formElement.addEventListener('submit', function(evt){
-          evt.preventDefault();
-      });
-
-      // Для каждой формы вызовем функцию setEventListeners, передав ей элемент формы
-      setEventListeners(formElement);
+      console.log('Enable Validation - function works!');
   });
 };
 
 enableValidation({
+  //В настройках: это - элементы
   formElement: '.form',
   inputElement: '.form__field',
   buttonElement: '.form__button-save',
-  //inactiveButtonClass: 'popup__button_disabled',
-  //inputErrorClass: 'form-error',
-  //errorClass: 'form-error_shown'
+  
+  //В настройках: это - добавляемые классы
+  disabledButtonClass: 'form__button-save_inactive',
+  enabledButtonClass: 'form__button-save_active',
+  inputErrorClass: 'form-error',
+  errorClass: 'form-error_shown'
 });
-*/
