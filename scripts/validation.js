2bea8963faa10c0f5d18ca398553f26f5e97ca00
fmkrom <fmkrom@yandex.ru>
2020-10-31 12:50:00 +0300

    refactoring: работа над функциями валидации

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,83 +2,70 @@
 
 //Функция: высветить невалидное поле - работает
 
-function showInputError(form, input){
-    
+function showInputError(form, input, parameters){
   const errorElement = form.querySelector(`#${input.id}-error`);
-  input.classList.add('form__field_invalid');
+  input.classList.add(parameters.invalidInputClass);
   errorElement.textContent = input.validationMessage;
-  errorElement.classList.add('form-error_shown');
-  console.log('function showInputError works!');
+  errorElement.classList.add(parameters.errorShownClass);
 };
 
-function hideInputError(form, input){
+function hideInputError(form, input, parameters){
   const errorElement = form.querySelector(`#${input.id}-error`);
-  input.classList.remove('form__field_invalid');
-  errorElement.classList.remove('form-error_shown');
+  input.classList.remove(parameters.invalidInputClass);
+  errorElement.classList.remove(parameters.errorShownClass);
   errorElement.textContent = '';
-  console.log('function hideInputError works!');
 }; 
 
-function isValid(form, input){
+function isValid(form, input, parameters){
     if (!input.validity.valid){
-      showInputError(form, input, input.validationMessage);
+      showInputError(form, input, parameters);
     } else {
-      hideInputError(form, input);
+      hideInputError(form, input, parameters);
     }
-    console.log('function isValid works!');
 };
 
-//Функция: вернуть при невалидности!
+//Функция: вернуть при невалидности
 function returnInvalidInput(input){
-  console.log('function returnInvalidInput works!');
   return input.some(function(input){
   return !input.validity.valid;
   });
 };
 
 //Переключение кнопки
-function toggleButtonState(inputsArray, button){
+function toggleButtonState(inputsArray, button, parameters){
   if (returnInvalidInput(inputsArray)){
-    button.classList.add('form__button-save_inactive');
+    button.classList.add(parameters.inactiveButtonClass);
     button.disabled = true;
   } else {
-    button.classList.remove('form__button-save_inactive');
+    button.classList.remove(parameters.inactiveButtonClass);
     button.disabled = false;
   }
-  console.log('function toggleButtonState works!');
 }; 
 
 //Функция: добавляем слушатели событий
-function setEventListeners(form){
-  const inputListArray = Array.from(form.querySelectorAll('.form__field'));
-  const buttonToToggle = form.querySelector('.form__button-save');
+function setEventListeners(form, parameters){
+  const inputListArray = Array.from(form.querySelectorAll(parameters.inputNodeElement));
+  const buttonToToggle = form.querySelector(parameters.buttonNodeElement);
   
   inputListArray.forEach(function(input){
       input.addEventListener('input', function(){
         isValid(form, input);
-        toggleButtonState(inputListArray, buttonToToggle);
+        toggleButtonState(inputListArray, buttonToToggle, parameters);
       });
   });
-  console.log('function setEventListeners works!');
 };
 
-function enableValidation({formNodeElement, inputNodeElement, buttonNodeElement, inactiveButtonClass, invalidInputClass, errorShownClass}){
-
-  const formList = Array.from(document.querySelectorAll(formNodeElement));
-
-    formList.forEach(function(formNodeElement){
-      
-      formNodeElement.addEventListener('submit', function(evt){
+function enableValidation(form, parameters){
+  const formList = Array.from(document.querySelectorAll(form));
+    formList.forEach(function(form){
+      form.addEventListener('submit', function(evt){
           evt.preventDefault();
       });
-
-      setEventListeners(formNodeElement);
+      setEventListeners(form, parameters);
     });
-
-    console.log('function enableValidation works!');
 };
 
-enableValidation({
+const validationParameters = {
   formNodeElement: '.form',
   inputNodeElement: '.form__field',
   buttonNodeElement: '.form__button-save',
@@ -86,4 +73,6 @@ enableValidation({
   inactiveButtonClass: 'form__button-save_inactive',
   invalidInputClass: 'form__field_invalid',
   errorShownClass: 'form-error_shown'
-});
+};
+
+enableValidation(validationParameters.formNodeElement, validationParameters);
